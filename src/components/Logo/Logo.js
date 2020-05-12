5af5369a8dc7272b58742040455e7263593377ca
marlonjf <marlon.jf12@gmail.com>
2020-05-12 00:13:49 -0300

    some components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import brain from './brain.png';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0 pa3' style={{textAlign: 'left'}}>
+      <img alt='brain' src={brain}/>
+    </div>
+  )
+}
+
+export default Logo;
